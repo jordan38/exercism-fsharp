07e0572a86a41e9de73bc33b61ca3fb2037ca6f7
jordan <jordan.content@asterion-int.com>
2017-08-08 16:35:10 +0200

    Leap Soumis
    modification de la version de nunit 2.6 pour effectuer les tests

--- leap/LeapYear.fs
+++ leap/LeapYear.fs
@@ -1,4 +1,18 @@
 ﻿module LeapYear
 
-let isLeapYear year = true
+let isDivideBy4 year = 
+    year % 4 = 0
+
+let isDivideBy100 year =
+    year % 100 = 0
+    
+let isDivideBy400 year =
+    year % 400 = 0
+
+let isLeapYear year = 
+    let isLeapYear = isDivideBy4 year
+    
+    if not isLeapYear then isLeapYear 
+    elif isDivideBy100 year then isDivideBy400 year
+    else true
 
